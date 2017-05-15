ec5e232b7df4949dbf72e1e7876c46a39486664e
Ranc58 <rancvova@gmail.com>
2017-05-15 12:03:00 +0300

    adding error exceptions, correcting functions

--- pprint_json.py
+++ pprint_json.py
@@ -2,28 +2,29 @@ import json
 import sys
 
 
-def start_program():
+def load_way_and_output_json_content():
     input_way = input('Please enter full way to JSON file '
                       'or type "exit" to close program: ')
-    load_data(input_way) if input_way != 'exit' else sys.exit()
+    pretty_print_json(load_data(input_way)) if input_way is not 'exit' else sys.exit()
 
 
 def load_data(input_way):
     try:
-        with open(input_way, 'r', encoding='UTF-8') as file_handler:
-            content_json = json.load(file_handler)
-            pretty_print_json(content_json)
-    except (ValueError, FileNotFoundError) as error:
-        print(error)
-    finally:
+        with open(input_way, 'r', ) as file_handler:
+            return json.load(file_handler)
+    except Exception:
+        return None
+
+
+def pretty_print_json(load_data):
+    if load_data is not None:
+        user_friendly_json = json.dumps(load_data, sort_keys=True,
+                                        indent=4, ensure_ascii=False)
+        print(user_friendly_json)
+    else:
+        print("Error! Please check JSON file and directory way")
         start_program()
 
 
-def pretty_print_json(content_json):
-    user_friendly_json = json.dumps(content_json, sort_keys=True,
-                                    indent=4, ensure_ascii=False)
-    print(user_friendly_json)
-
-
 if __name__ == '__main__':
-    start_program()
+    load_way_and_output_json_content()
